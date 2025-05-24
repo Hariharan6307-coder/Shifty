4e6b33da26beb2e37863f0ba628104e25d280ff1
Hariharan <hariharan.m6307@gmail.com>
2025-05-24 19:24:43 +0530

    Deleted all the movement code and the player can rotate now :)

--- scripts/main.js
+++ scripts/main.js
@@ -6,22 +6,34 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800
 canvas.height = 600
 
-const player = new Player("../images/player/playerMove1.png", canvas.width / 2, canvas.height / 2);
+const gridSize = 50;
 
-let mouseX = 0;
-let mouseY = 0;
-let mousePos = {};
-canvas.addEventListener("mousemove", ((event) => {
-  const rect = canvas.getBoundingClientRect();
-  mouseX = event.clientX - rect.left;
-  mouseY = event.clientY - rect.top;
-  mousePos = {x: mouseX, y: mouseY};
-}));
+function drawGrid() {
+  ctx.strokeStyle = "#cccccc";
+  ctx.lineWidth = 1;
+
+  for (let x = 0; x <= canvas.width; x += gridSize) {
+    ctx.beginPath();
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, canvas.height);
+    ctx.stroke();
+  }
+
+  for (let y = 0; y <= canvas.height; y += gridSize) {
+    ctx.beginPath();
+    ctx.moveTo(0, y);
+    ctx.lineTo(canvas.width, y);
+    ctx.stroke();
+  }
+}
+
+const player = new Player("../images/player/player.png", canvas.width / 2, canvas.height / 2);
 
 function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  player.update(ctx, mousePos);
+  drawGrid();
+  player.update(ctx);
 
   requestAnimationFrame(gameLoop);
 }
--- scripts/player.js
+++ scripts/player.js
@@ -5,29 +5,55 @@ export class Player {
     this.pos = {x: x, y: y}
 
     this.speed = 5;
+    this.rotationAngle = 0;
+
+    this.direction = {
+      forward: false,
+      right: false,
+      left: false
+    }
+
+    this.keys = {};
+    document.addEventListener('keydown', (e) => {
+      this.keys[e.key] = true;
+    });
+    document.addEventListener('keyup', (e) => {
+      this.keys[e.key] = false;
+    })
   }
 
-  move(pos) {
-    let oldPos = this.pos;
-    let newPos = pos;
-
-    let dx = newPos.x - oldPos.x;
-    let dy = newPos.y - oldPos.y;
-
-    let hyp = Math.hypot(dx, dy);
-
-    if (hyp > 5) {
-      this.pos.x += (dx / hyp) * this.speed;
-      this.pos.y += (dy / hyp) * this.speed;
+  move() {
+    this.direction.forward = false;
+    this.direction.down = false;
+    this.direction.right = false;
+    this.direction.left = false;
+    if (this.keys['w'] || this.keys['W']) {
+      this.direction.forward = true;
     }
+    if (this.keys['a'] || this.keys['A']) {
+      this.direction.left = true;
+    }
+    if (this.keys['d'] || this.keys['D']) {
+      this.direction.right = true;
+    }
+
+    if (this.direction.left) this.rotationAngle -= 2;
+    if (this.direction.right) this.rotationAngle += 2;
+    
   }
 
   draw(ctx) {
-    ctx.drawImage(this.image, (this.pos.x - (this.image.width / 2)), this.pos.y);
+    ctx.drawImage(this.image, this.pos.x - this.image.width / 2, this.pos.y - 56);
+
+    // ctx.save();
+    // ctx.translate(this.pos.x, this.pos.y);
+    // ctx.rotate(this.rotationAngle * Math.PI / 180);
+    // ctx.drawImage(this.image, -this.image.width / 2, -56);
+    // ctx.restore();
   }
 
-  update(ctx, mousePos) {
+  update(ctx) {
     this.draw(ctx)
-    this.move(mousePos)
+    this.move()
   }
 }
