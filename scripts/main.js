14583a3e07d827b9f849bdae3abdb2b537c5f2b9
Hariharan <hariharan.m6307@gmail.com>
2025-05-31 21:54:04 +0530

    Made the main.js object oriented and got stuff sorted to work with

--- scripts/main.js
+++ scripts/main.js
@@ -9,50 +9,69 @@ canvas.height = 600
 
 const gridSize = 50;
 
-function drawGrid() {
-  ctx.strokeStyle = "#cccccc";
-  ctx.lineWidth = 1;
+class MainGame {
+  constructor() {
+    this.player = new Player(ctx, "../images/player/player.png", "../images/player/eye.png");
+    this.enemyGroup = [];
 
-  for (let x = 0; x <= canvas.width; x += gridSize) {
-    ctx.beginPath();
-    ctx.moveTo(x, 0);
-    ctx.lineTo(x, canvas.height);
-    ctx.stroke();
+    let enemy = new Enemy(ctx, this.player, 400, 300);
+    this.enemyGroup.push(enemy);
+
+    this.getMousePos();
   }
 
-  for (let y = 0; y <= canvas.height; y += gridSize) {
-    ctx.beginPath();
-    ctx.moveTo(0, y);
-    ctx.lineTo(canvas.width, y);
-    ctx.stroke();
+  drawGrid() {
+    ctx.strokeStyle = "#cccccc";
+    ctx.lineWidth = 1;
+
+    for (let x = 0; x <= canvas.width; x += gridSize) {
+      ctx.beginPath();
+      ctx.moveTo(x, 0);
+      ctx.lineTo(x, canvas.height);
+      ctx.stroke();
+    }
+
+    for (let y = 0; y <= canvas.height; y += gridSize) {
+      ctx.beginPath();
+      ctx.moveTo(0, y);
+      ctx.lineTo(canvas.width, y);
+      ctx.stroke();
+    }
+  }
+
+  getMousePos() {
+    this.mousePos = {x: 0, y: 0};
+    canvas.addEventListener("mousemove", (event) => {
+      const rect = canvas.getBoundingClientRect();
+      this.mousePos.x = event.clientX - rect.left;
+      this.mousePos.y = event.clientY - rect.top;
+    });
+  }
+
+  checkPlayerBulletCollisions(timeStamp) {
+    this.enemyGroup.forEach((enemy) => {
+      enemy.update(timeStamp);
+      this.player.checkBulletCollisions(enemy.bulletGroup);
+    });
+  }
+
+  update(timeStamp) {
+    ctx.fillStyle = "#1C253C";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    this.drawGrid();
+
+    this.checkPlayerBulletCollisions(timeStamp);
+
+    this.player.update(this.mousePos);
+  }
+
+  gameLoop = (timeStamp) => {
+    this.update(timeStamp)
+    requestAnimationFrame(this.gameLoop);
   }
 }
 
-let mouseX = 0;
-let mouseY = 0;
-let mousePos = {};
-canvas.addEventListener("mousemove", (event) => {
-  const rect = canvas.getBoundingClientRect();
-  mouseX = event.clientX - rect.left;
-  mouseY = event.clientY - rect.top;
-  mousePos = {x: mouseX, y: mouseY}
-});
+const mainGame = new MainGame();
+mainGame.gameLoop();
 
-
-const player = new Player(ctx, "../images/player/player.png", "../images/player/eye.png");
-const enemy = new Enemy(ctx, player, 400, 300);
-
-function gameLoop(timeStamp) {
-  ctx.fillStyle = "#1C253C";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  drawGrid();
-  enemy.update(timeStamp);
-  player.update(mousePos);
-
-  player.checkBulletCollisions(enemy.bulletGroup);
-
-  requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
